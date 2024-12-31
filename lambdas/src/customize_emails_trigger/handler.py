628de3773f9259e26d5d03f6d5c406a1e4ff82c0
Anthony Viera <anthonyviera4@gmail.com>
2024-12-30 19:49:22 -0600

    what about this

--- lambdas/src/customize_emails_trigger/handler.py
+++ lambdas/src/customize_emails_trigger/handler.py
@@ -28,7 +28,7 @@ def lambda_handler(event, context):
         event["response"]["emailSubject"] = "Reset Your Password"
         event["response"]["emailMessage"] = (
             f"Hello {first_name}, <br><br>"
-            f"We received a request to reset your password. Your reset code is {code}. You can also click the link below to reset your password.<br><br>"
+            f"We received a request to reset your password. Click the link below to reset your password.<br>"
             f"<a href='{get_website_url()}/?reset=true&username={urllib.parse.quote(username)}&code={code}'>Reset Password</a><br><br>"
             f"If you did not request this password reset, please ignore this email."
         )
