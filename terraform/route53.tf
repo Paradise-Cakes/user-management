5b7c43457d33442f8bd405e7df384dfc9892f96f
Anthony Viera <36007311+Anthony9698@users.noreply.github.com>
2024-12-31 19:30:40 -0600

    Revert "Revert "No api (#15)" (#16)"
    This reverts commit 441bd6dec1d687041c836fa505b1027a7a133a54.

--- terraform/route53.tf
+++
@@ -1,7 +0,0 @@
-data "aws_route53_zone" "users_api" {
-  name = var.environment == "prod" ? "users-api.megsparadisecakes.com" : "users-dev-api.megsparadisecakes.com"
-}
-
-data "aws_route53_zone" "paradise_cakes" {
-  name = var.environment == "prod" ? "megsparadisecakes.com" : "dev.megsparadisecakes.com"
-}
