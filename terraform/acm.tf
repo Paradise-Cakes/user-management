afe7154df7223462fb6a50666229cc65a0fcd9cd
Anthony Viera <anthonyviera4@gmail.com>
2024-12-31 23:55:45 -0600

    how about this

--- terraform/acm.tf
+++
@@ -1,5 +0,0 @@
-data "aws_acm_certificate" "users_api" {
-  domain      = var.environment == "prod" ? "users-api.megsparadisecakes.com" : "users-dev-api.megsparadisecakes.com"
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
-}
