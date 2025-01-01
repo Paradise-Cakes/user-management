96ad0b66652fd093283770b43cd4fd8431a6ba2f
Anthony Viera <anthonyviera4@gmail.com>
2024-12-31 23:44:26 -0600

    BURN IT ALLL

--- lambdas/src/lib/aws_resources.py
+++ lambdas/src/lib/aws_resources.py
@@ -1,13 +1,4 @@
 import os
 
-
-def get_cognito_app_client_id():
-    return os.environ.get("COGNITO_APP_CLIENT_ID")
-
-
-def get_cognito_user_pool_id():
-    return os.environ.get("COGNITO_USER_POOL_ID")
-
-
 def get_website_url():
     return os.environ.get("WEBSITE_URL", "http://localhost:5173")
